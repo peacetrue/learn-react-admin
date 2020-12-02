cc99dde5e07c08b583b8f83826aea3710af40fdd
xiayx <xiayouxue@gold32.com>
2020-12-02 18:12:22 +0800

    登陆页

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -6,10 +6,11 @@ import {PostCreate, PostEdit, PostList} from './posts';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name="users" list={UserList}  icon={UserIcon}/>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}  icon={PostIcon}/>
     </Admin>
