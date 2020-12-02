d6590f5dcdcaddb6c4bb257ee3303765a0316ce4
xiayx <xiayouxue@gold32.com>
2020-12-02 18:26:42 +0800

    自定义 dataProvider

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,18 +1,17 @@
 import * as React from "react";
 import {Admin, Resource} from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
 import {UserList} from './users';
 import {PostCreate, PostEdit, PostList} from './posts';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
+import dataProvider from './dataProvider';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
-        <Resource name="users" list={UserList}  icon={UserIcon}/>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}  icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     </Admin>
 );
 
