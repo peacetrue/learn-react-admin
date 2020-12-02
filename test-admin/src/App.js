82ae0dd6053a2995a70fe75702bdd1e86de6b361
xiayx <xiayouxue@gold32.com>
2020-12-02 18:09:25 +0800

    App.js 引用 Dashboard

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -3,12 +3,15 @@ import {Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import {UserList} from './users';
 import {PostCreate, PostEdit, PostList} from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={UserList}/>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        <Resource name="users" list={UserList}  icon={UserIcon}/>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}  icon={PostIcon}/>
     </Admin>
 );
 
